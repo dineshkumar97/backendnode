71904abceeec2720562c5ba4bc15dd00a35fdc11
dineshkumar97 <dineshkumarppn07@gmail.com>
2026-09-13 12:17:37 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,32 @@
+import dotenv from "dotenv";
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import { setServers } from "node:dns/promises";
+// import userEmailRouter from "./src/routes/userEmailRouter.js";
+setServers(["1.1.1.1", "8.8.8.8"]);
+dotenv.config();
+const mongooseString = process.env.DATABASE_URL;
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+
+
+// app.use("/api/user-email", userEmailRouter);
+
+
+mongoose.connect(mongooseString)
+  .then(() => {
+    console.log('Database connected successfully');
+  })
+  .catch((err) => {
+    console.log('Error connecting to database: ' + mongooseString);
+    console.log('Error received = ' + err);
+  });
+
+app.listen(3000, () => {
+    console.log('Server started 3000')
+})
+
+
