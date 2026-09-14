5020f617180c9b751dcfc4dac8a6147286ce8c6b
dineshkumar97 <dineshkumarppn07@gmail.com>
2026-09-15 01:04:13 +0530

    again push server code

--- index.js
+++ index.js
@@ -1,35 +1,35 @@
-import dotenv from "dotenv";
-import express from "express";
-import mongoose from "mongoose";
-import cors from "cors";
-import { setServers } from "node:dns/promises";
-import userDetailsRouter from "./src/routes/userDetailsRouter.js";
-setServers(["1.1.1.1", "8.8.8.8"]);
-dotenv.config();
-const mongooseString = process.env.DATABASE_URL;
-const PORT = process.env.PORT || 3000;
-const app = express();
-app.use(cors());
-app.use(express.json());
+// import dotenv from "dotenv";
+// import express from "express";
+// import mongoose from "mongoose";
+// import cors from "cors";
+// import { setServers } from "node:dns/promises";
+// import userDetailsRouter from "./src/routes/userDetailsRouter.js";
+// setServers(["1.1.1.1", "8.8.8.8"]);
+// dotenv.config();
+// const mongooseString = process.env.DATABASE_URL;
+// const PORT = process.env.PORT || 3000;
+// const app = express();
+// app.use(cors());
+// app.use(express.json());
 
 
-app.use("/api-learn/user", userDetailsRouter);
+// app.use("/api-learn/user", userDetailsRouter);
 
 
 
-mongoose.connect(mongooseString)
-  .then(() => {
-    console.log('Database connected successfully');
-  })
-  .catch((err) => {
-    console.log('Error received = ' + err);
-  });
+// mongoose.connect(mongooseString)
+//   .then(() => {
+//     console.log('Database connected successfully');
+//   })
+//   .catch((err) => {
+//     console.log('Error received = ' + err);
+//   });
 
-app.listen(PORT, () => {
-    console.log(`Server started on port ${PORT}`);
-})
+// app.listen(PORT, () => {
+//     console.log(`Server started on port ${PORT}`);
+// })
 
-/* 
+
 import dotenv from "dotenv";
 import express from "express";
 import mongoose from "mongoose";
@@ -66,4 +66,3 @@ app.get("/", (req, res) => {
 });
 
 export const handler = serverless(app);
-*/
